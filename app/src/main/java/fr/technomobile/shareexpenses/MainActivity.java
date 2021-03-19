0aa0fc5e8cce5779a21988a07c72ba592f39c450
isoubedda <issam.oubedda@etu.uca.fr>
2021-03-19 18:03:50 +0100

    page 2

--- app/src/main/java/fr/technomobile/shareexpenses/MainActivity.java
+++ app/src/main/java/fr/technomobile/shareexpenses/MainActivity.java
@@ -3,15 +3,30 @@ package fr.technomobile.shareexpenses;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.addGroup = findViewById(R.id.btnNewGroup);
+
+        addGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newGroupActivity = new Intent(getApplicationContext(),NewGroupActivity.class);
+                startActivity(newGroupActivity);
+                finish();
+            }
+        });
     }
 
 }
